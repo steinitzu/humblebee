90712bc5d7b4dcf1d2957b5474c29cbdac2e0561
steinitzu <steini90@gmail.com>
2012-04-28 04:02:19 +0000

    bleh

--- src/romdb/romlog.py
+++ src/romdb/romlog.py
@@ -7,6 +7,12 @@ streamhdlr.setFormatter(formatter)
 rom_log.addHandler(streamhdlr)
 
 
+test_log = logging.getLogger('testlog')
+file_handler = logging.FileHandler('testlog.log',  mode='a')
+file_handler.setFormatter(formatter)
+test_log.addHandler(file_handler)
+test_log.setLevel(logging.DEBUG)
+
 
 def log_info(text):
     rom_log.info(text)
--- src/test.py
+++ src/test.py
@@ -5,7 +5,22 @@ from romdb.romlog import *
 
 
 def non_destructive_test():
-    tvdirparser.scan_tv_source('/media/boneraper/+incoming')
+
+    scanner = tvdirparser.TVSourceScanner('/media/boneraper/+incoming')
+    scanner.scan_tv_source()
+    rom_log.info('\nPROPER EPS\n\n')
+    [rom_log.info(ep) for ep in scanner.proper_episodes]
+    rom_log.info( '\n\nUNPARSABLE EPS\n')
+    [rom_log.info(ep) for ep in scanner.unparseable_eps]
+    rom_log.info('\n\nSINGLE EP DIRS\n')
+    [rom_log.info(ep) for ep in scanner.single_ep_dirs]
+
+    test_log.info('Number of proper_episodes = %d' % len(scanner.proper_episodes))
+    test_log.info('Number of unparseable_eps = %d' % len(scanner.unparseable_eps))
+    test_log.info('Number of single_ep_dirs = %d' % len(scanner.single_ep_dirs))    
+
+    
+    
 
 if __name__ == "__main__":
     rom_log.setLevel(logging.DEBUG)
