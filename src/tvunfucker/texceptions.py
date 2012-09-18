8b6d8c9457db55ba060423e657eff9a7dbbc7d36
steini <steini90@gmail.com>
2012-09-18 05:01:05 +0000

    SeasonNotFoundError, EpisodeNotFoundError

--- src/tvunfucker/texceptions.py
+++ src/tvunfucker/texceptions.py
@@ -28,3 +28,15 @@ class ShowNotFoundError(TVUFError):
     def __init__(self, msg, *args, **kwargs):
         msg = 'Show \'%s\' was not found on the TVDB' % msg
         TVUFError.__init__(self, msg, *args, **kwargs)
+
+class EpisodeNotFoundError(TVUFError):
+    def __init__(self, sow, season, ep, *args, **kwargs):
+        msg = 'Series \'%s\', season %s, episode %s was not found on the TVDB' % (
+            sow, season, ep)
+        TVUFError.__init__(self, msg, *args, **kwargs)
+
+class SeasonNotFoundError(TVUFError):
+    def __init__(self, show, season, *args, **kwargs):
+        msg = 'Series \'%s\', season %s was not found on the TVDB' % (
+            show, season)
+        TVUFError.__init__(self, msg, *args, **kwargs)        
