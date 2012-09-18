d1d803db9d1b65ce5e7184a08119001f6009171f
steini <steini90@gmail.com>
2012-09-18 03:34:20 +0000

    moved the stupid type_safe function here

--- src/tvunfucker/util.py
+++ src/tvunfucker/util.py
@@ -4,6 +4,8 @@
 from datetime import datetime
 from time import mktime
 
+from texceptions import InvalidArgumentError
+
 
 def zero_prefix_int(num):
     """
@@ -38,3 +40,30 @@ def ensure_utf8(value):
     if isinstance(value, unicode):
         return value
     return value.decode('utf-8')
+
+
+
+def type_safe(
+    arg,
+    expected_type,
+    arg_num=0,
+    error_message=None
+    ):
+    """
+    A pseudo type safety function.\n
+    arg: the object you want to check\n
+    expected_type: the desired type of arg\n
+    arg_num=0: the number of this argument in the calling function\n
+    error_message=None: Message raised in case the type is wrong.
+    Defaults to 'Invalid type for argument %d. Got \'%s\' expected \'%s\'.'\n
+    \n
+    Raises an InvalidArgumentError or returns True
+    """    
+    if not error_message:
+        error_message = (
+            'Invalid type for argument %d. Got \'%s\' expected \'%s\'.' %
+            (arg_num, type(arg), expected_type)
+            )        
+    if not isinstance(arg, expected_type):
+        raise (InvalidArgumentError(error_message))
+    return True
