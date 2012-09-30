8f23b0f24359f40e7a06e6279eef42fa68908329
steini <steini90@gmail.com>
2012-09-30 01:49:10 +0000

    get_metadata method and symlinks

--- src/tvunfucker/thefuse.py
+++ src/tvunfucker/thefuse.py
@@ -23,11 +23,12 @@ class FileSystem(LoggingMixIn, Operations):
     filename_mask_ep = (
        ' %(series_title)s s%(season_number)se%(ep_number)s%(extra_ep_number)s %(title)s%(ext)s'
        )
-    filename_mask_season = 's%(season_number)s'
+    filename_mask_season = 's%(season_number)s'    
     
     def __init__(self, db):
         #files will be taken straight from db, no stupid shit
         self.db = db
+        self.symlinks = {} #source : target
         pass
 
     def readdir(self, path, fh):
@@ -67,6 +68,65 @@ class FileSystem(LoggingMixIn, Operations):
                 ret.append(self.filename_mask_ep % row)
             return ret
 
+    def symlink(self, target, name):
+        target = 1
+
+    def readlink(self, path):
+        data = self.get_metadata(path)
+        #a keyerror here should NEVER happen, if it does, you made programming error derp
+        realpath = data[0]['file_path']
+        log.debug('Real path of %s = %s', path, realpath)
+        return realpath        
+
+    def get_metadata(self, path):
+        """
+        Returns the episode from the given path, season or series info
+        as a dict. See the db schema for column info.
+        """
+        pathpcs = self._split_path(path)        
+
+        def check_rows(rows):
+            if not rows:
+                raise FuseOSError(ENOENT)
+            elif len(rows) > 1:
+                raise WTFException(
+                    'filename %s has more than one candidate' % pathpcs[0]
+                    )
+            return rows[0]        
+
+        if path == '/': #root
+            return None
+
+        elif path == '/_unparsed':
+            raise FuseOSError(ENOENT)
+
+        elif len(pathpcs) == 1: #series_dir
+            rows = self.db.get_series_plural(
+                title=pathpcs[0]
+                )
+            row = check_rows(rows)
+            return row, 'series'
+        elif len(pathpcs) == 2: #season dir
+            f = parse_file(path)
+
+            log.debug('Prased ep: %s', f)
+            rows = self.db.get_seasons(
+                series_title = pathpcs[0],
+                season_number = f['season_num']
+                )
+            log.debug('Found seasons: %s', rows)
+            return check_rows(rows), 'season'
+        elif len(pathpcs) == 3: #episode file
+            f = parse_file(path)
+            log.debug('Prased ep: %s', f)
+            rows = self.db.get_episodes(
+                season_number=f['season_num'],
+                series_title=pathpcs[0],
+                ep_number=f['ep_num']
+                )
+            return check_rows(rows), 'episode'        
+        
+
     def getattr(self, path, fh=None):
         log.debug('path: %s', path)
 
@@ -87,6 +147,15 @@ class FileSystem(LoggingMixIn, Operations):
                 'st_nlink' : 2
                 }
 
+        symlinkmode = {
+            'st_mode' : (S_IFLNK | 755),
+            'st_ctime' : now,
+            'st_mtime' : now,
+            'st_atime' : now,
+            'st_nlink' : 2
+            }
+            
+        """
         pathpcs = self._split_path(path)
 
         def check_rows(rows):
@@ -97,15 +166,19 @@ class FileSystem(LoggingMixIn, Operations):
                     'filename %s has more than one candidate' % pathpcs[0]
                     )
             return rows[0]
+        """
             
         def make_ret(row, mode='dir'):
-            l = dirmode if mode=='dir' else filemode
+            l = None
+            if mode == 'dir' : l = dirmode
+            elif mode == 'file': l = filemode
+            elif mode == 'symlink' : l = symlinkmode
             l.update({
                     'st_ctime':timestamp(row['created_time']),
                     'st_mtime':timestamp(row['modified_time'])
                     })
             return l
-
+        """
         if path == '/': #root
             return dirmode
 
@@ -136,8 +209,15 @@ class FileSystem(LoggingMixIn, Operations):
                 series_title=pathpcs[0],
                 ep_number=f['ep_num']
                 )
-            return make_ret(check_rows(rows), 'file')
-
+            return make_ret(check_rows(rows), 'symlink')
+        """
+        if path == '/': return dirmode
+        data = self.get_metadata(path)
+        if data[1] == 'series' or data[1] == 'season':
+            return make_ret(data[0], 'dir')
+        elif data[1] == 'episode':
+            return make_ret(data[0], 'symlink')
+        raise FuseOSError(ENOENT)
         
 
     def chmod(self, path, mode):
