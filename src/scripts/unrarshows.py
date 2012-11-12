ca738aca0987322202bb2fd5d0ae9eb4499ebef2
steini <steini90@gmail.com>
2012-11-12 06:20:06 +0000

    some stuffs

--- src/scripts/unrarshows.py
+++ src/scripts/unrarshows.py
@@ -23,14 +23,22 @@ def main(tvdir):
         os.chdir(ep)
         globr1 = glob('*.r01')
         globrar = glob('*.rar')
-        log.debug('%s: %s, %s', ep, globrar, globr1)
+        #log.debug('%s: %s, %s', ep, globrar, globr1)
         if globrar and globr1:
             rareps.append(ep)
-        if (globr1 or globrar) and not globrar or not globr1:
+            log.info(
+                'Found rarred episode: %s', 
+                os.path.relpath(ep, start=tvdir)
+                )
+        if (globr1 or globrar) and (not globrar or not globr1):
+            log.info(
+                'Found half-rarred episode: %s', 
+                os.path.relpath(ep, start=tvdir)
+                )            
             oneglob.append(ep)
+    #log.debug('\n'.join(e for e in oneglob))
     log.info('There were %s rared episodes.', len(rareps))    
-    log.info('There were %s singleglob episodes.', len(oneglob))
-    log.debug('\n'.join(e for e in oneglob))
+    log.info('There were %s singleglob episodes.', len(oneglob))    
 
 if __name__ == "__main__":
     main(sys.argv[1])
