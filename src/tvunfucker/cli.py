977ba23b3af2c4be1769a9a8fe04b54ebf2b08cb
steini <steini90@gmail.com>
2012-12-28 00:45:45 +0000

    new options to cli 'reset_database' and 'update_database'

--- src/tvunfucker/cli.py
+++ src/tvunfucker/cli.py
@@ -1,22 +1,28 @@
 from argparse import ArgumentParser
 
 from .importer import Importer
+from . import appconfig
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument(
-        '-l', '--log-file', dest='log_file', 
-        help='Path to log file.'
-        )
-    parser.add_argument(
         'directory', 
         help='Your TV directory. Can be any directory containing TV shows.'
         )
     parser.add_argument(
-        '-r', '--reset-database', dest='reset_database',
+        '-u', '--update', dest='update_database',
+        action='store_true', default=True,
+        help='Only import episodes which don\'t already exist in database. (Default)'
+        )
+    parser.add_argument(
+        '-r', '--reset', dest='reset_database',
         action='store_true', default=False,
-        help='Overwrite existing database (if any) in directory.'
+        help='Overwrite existing database (if any) in directory.(overrides --update)'
+        )
+    parser.add_argument(
+        '-l', '--log-file', dest='log_file', 
+        help='Path to log file.'
         )
     parser.add_argument(
         '-v', '--verbosity', dest='log_level',
@@ -27,6 +33,19 @@ def main():
         )
 
     args = parser.parse_args()
+
+    argsd = {}
+    argsd['logging'] = {
+        'level':args.log_level,
+        'filename':args.log_file
+        }
+    argsd['database'] = {
+        'reset':args.reset_database,
+        'update':args.update_database
+        }
+
+    appconfig.import_to_runtime_parser(argsd)
+    
 
 if __name__ == '__main__':
     main()
