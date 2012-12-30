31e500a034996cd8e948c4d9e480dc29603417b0
steini <steini90@gmail.com>
2012-12-30 21:57:19 +0000

    Revert "added bingpy as a submodule"
    This reverts commit 070d34a8df27c7229759e63bb72e090e5896b5d3.
    
    submodules are silly

--- src/tvunfucker/tvdbwrapper.py
+++ src/tvunfucker/tvdbwrapper.py
@@ -13,7 +13,7 @@ from .texceptions import ShowNotFoundError, SeasonNotFoundError, EpisodeNotFound
 from .texceptions import NoIdInURLError, IncompleteEpisodeError
 from . import __pkgname__
 from . import appconfig as cfg
-from .bingpy.bing import Bing
+from bing import Bing
 from .dbguy import Episode
 
 log = logging.getLogger(__pkgname__)
