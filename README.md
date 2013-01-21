907ffff0b853672206edb0125986fb644895c5fc
steini <steini90@gmail.com>
2013-01-21 03:02:36 +0000

    version no bump

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Humblebee  (version 1.06) #
+# Humblebee  (version 1.07) #
 
 Humblebee is a scraper and renamer for TV shows.
 It handles many naming schemes and the most odd directory structures.
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='humblebee',
-    version='1.06',
+    version='1.07',
     description='A scraper for TV shows.',
     author='Steinthor Palsson',
     author_email='steinitzu@gmail.com',
