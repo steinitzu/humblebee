ec3055178c3a1988b0e7de0680fa8b5fec5fda18
steini <steini90@gmail.com>
2013-05-10 03:23:26 +0000

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='humblebee',
-    version='1.10',
+    version='1.101',
     description='A scraper for TV shows.',
     author='Steinthor Palsson',
     author_email='steinitzu@gmail.com',
